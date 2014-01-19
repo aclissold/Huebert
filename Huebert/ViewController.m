f1885d1c7692f964d26ef4145220d7e5243037be
Andrew Clissold <andrewclissold@gmail.com>
2014-01-19 11:10:46 -0500

    Change hues as if spinning the device on a desk

--- Huebert/ViewController.m
+++ Huebert/ViewController.m
@@ -7,16 +7,49 @@
 //
 
 #import "ViewController.h"
+#import <CoreMotion/CoreMotion.h>
 
 @interface ViewController ()
+
+@property (strong, nonatomic) CMMotionManager* motionManager;
+@property (weak, nonatomic) IBOutlet UILabel *xLabel;
+@property (weak, nonatomic) IBOutlet UILabel *yLabel;
+@property (weak, nonatomic) IBOutlet UILabel *zLabel;
 
 @end
 
 @implementation ViewController
 
+const float kRate = 0.05f;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    self.motionManager = [CMMotionManager new];
+    [self.motionManager setDeviceMotionUpdateInterval:kRate];
+    [self.motionManager startDeviceMotionUpdatesToQueue:[NSOperationQueue mainQueue]
+                                            withHandler:
+     ^(CMDeviceMotion *deviceMotion, NSError *error)
+     {
+//         NSString *x = [[NSString alloc] initWithFormat:@"%.02f", deviceMotion.attitude.roll];
+//         self.xLabel.text = x;
+//         NSString *y = [[NSString alloc] initWithFormat:@"%.02f", deviceMotion.attitude.pitch];
+//         self.yLabel.text = y;
+//         NSString *z = [[NSString alloc] initWithFormat:@"%.02f", deviceMotion.attitude.yaw];
+//         self.zLabel.text = z;
+
+         float hue = deviceMotion.attitude.yaw / M_PI;
+         if (hue < 0) { hue *= -1; }
+
+         [UIView animateWithDuration:kRate animations:
+          ^{
+              [self.view setBackgroundColor:[UIColor colorWithHue:hue
+                                                       saturation:1
+                                                       brightness:1
+                                                            alpha:1]];
+          }];
+     }];
 }
 
 @end
